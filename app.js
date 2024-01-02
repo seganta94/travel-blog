984bba960772519d72b2a81596198fc57cf66aee
Eduardo Segantine <eduardo.segantine@gmail.com>
2024-01-02 17:54:41 -0300

    fixed destinations

--- app.js
+++ app.js
@@ -8,55 +8,52 @@ app.use(express.static('public'));
 app.use('/public/images/', express.static('./public/images'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/countries', (req, res) => {
-//   const countries = [
-//     'Argentina',
-//     'Austria',
-//     'Bahamas',
-//     'Belgium',
-//     'Bolivia',
-//     'Brazil',
-//     'Canada',
-//     'Chile',
-//     'Colombia',
-//     'Costa Rica',
-//     'Croatia',
-//     'Czechia',
-//     'Denmark',
-//     'Ecuador',
-//     'England',
-//     'Estonia',
-//     'Finland',
-//     'France',
-//     'Germany',
-//     'Hungary',
-//     'Ireland',
-//     'Italy',
-//     'Latvia',
-//     'Lithuania',
-//     'Luxembourg',
-//     'Mexico',
-//     'Monaco',
-//     'Morocco',
-//     'Netherlands',
-//     'Norway',
-//     'Peru',
-//     'Poland',
-//     'Portugal',
-//     'Qatar',
-//     'Slovakia',
-//     'Slovenia',
-//     'Spain',
-//     'Switzerland',
-//     'Turkey',
-//     'United Arab Emirates',
-//     'United States',
-//     'Uruguay',
-//     'Venezuela'
-//   ];
-//   res.render('destinations', { countries });
-//   next();
-// });
+const countries = [
+    'Argentina',
+    'Austria',
+    'Bahamas',
+    'Belgium',
+    'Bolivia',
+    'Brazil',
+    'Canada',
+    'Chile',
+    'Colombia',
+    'Costa Rica',
+    'Croatia',
+    'Czechia',
+    'Denmark',
+    'Ecuador',
+    'England',
+    'Estonia',
+    'Finland',
+    'France',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Italy',
+    'Latvia',
+    'Lithuania',
+    'Luxembourg',
+    'Mexico',
+    'Monaco',
+    'Morocco',
+    'Netherlands',
+    'Norway',
+    'Peru',
+    'Poland',
+    'Portugal',
+    'Qatar',
+    'Slovakia',
+    'Slovenia',
+    'Spain',
+    'Switzerland',
+    'Turkey',
+    'United Arab Emirates',
+    'United States',
+    'Uruguay',
+    'Venezuela'
+];
+
 
 app.get("/", (req, res) => {
   res.render('index.ejs');
@@ -87,7 +84,7 @@ app.get("/blog/jericoacoara", (req, res) => {
 });
 
 app.get("/destinations", (req, res) => {
-  res.render('destinations.ejs');
+  res.render('destinations.ejs', { countries });
 });
 
 app.get("/about", (req, res) => {
